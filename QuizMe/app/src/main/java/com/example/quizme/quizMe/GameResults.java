7145790264f1450d0ba347759814fb2d2de8a6c1
emmaj <emmaj@siminn.is>
2020-04-06 15:31:31 +0000

    plz work

--- QuizMe/app/src/main/java/com/example/quizme/quizMe/GameResults.java
+++ QuizMe/app/src/main/java/com/example/quizme/quizMe/GameResults.java
@@ -1,11 +1,15 @@
 package com.example.quizme.quizMe;
 
-import java.util.List;
-
 public class GameResults {
-    int id, score;
-    String category;
-   // TODO List<Question, Boolean> correctlyAnswered;
+    int id, score, userAnswer;
+    String category, correctAnswer;
+
+    public GameResults(String category, Integer userAnswer, String correctAnswer) {
+        this.category = category;
+        this.userAnswer = userAnswer;
+        this.correctAnswer = correctAnswer;
+    }
+    // TODO List<Question, Boolean> correctlyAnswered;
 
     public void setId(int id) {
         this.id = id;
@@ -28,6 +32,14 @@ public class GameResults {
     public String getCategory() {
         return category;
     }
+
+    public Integer getUserAnswers() { return userAnswer; }
+
+    public void setUserAnswers(Integer userAnswer) { this.userAnswer = userAnswer; }
+
+    public void setCorrectAnswer(String correctAnswer) { this.correctAnswer = correctAnswer; }
+
+    public String getCorrectAnswer() { return correctAnswer; }
 
     // TODO getta og setta correctlyAnswered
 }
